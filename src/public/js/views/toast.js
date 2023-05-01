55b2d44347f0e0e43c9b475f7e6bae11a13dbfd8
Duy <truongvanduy09072002@gmail.com>
2023-05-01 17:00:28 +0700

    complete toast message UI

---
+++ src/public/js/views/toast.js
@@ -0,0 +1,25 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   function toast({
+//     title = '',
+//     message = '',
+//     type = 'success',
+//     duration = 3000,
+//   }) {
+//     const toast = document.createElement('div');
+//     toast.classList.add('toast');
+//     toast.innerHTML = /* html */ `
+//     <div class="toast toast-${type}">
+//       <div class="toast-icon">
+//         <i class="fa-solid fa-circle-check"></i>
+//       </div>
+//       <div class="toast-body">
+//         <div class="toast-title">${title}</div>
+//         <div class="toast-msg">${message}</div>
+//       </div>
+//       <div class="toast-close">
+//         <i class="fa-solid fa-xmark"></i>
+//       </div>
+//     </div>
+//     `;
+//   }
+// });
